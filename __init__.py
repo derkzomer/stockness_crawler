b3d133e18a4da1cb5a218d572e20f9cce09decfa
root <root@ubuntu.members.linode.com>
2018-01-27 16:51:36 +0000

    latest commit

--- __init__.py
+++ __init__.py
@@ -2,11 +2,13 @@ from flask import Flask,render_template,request,redirect
 import csv,time
 
 app = Flask(__name__)
+#app.run(host='0.0.0.0')
+#app.config['DEBUG'] = True
 
-app.config['UPLOAD_FOLDER'] = 'uploads/'
+FLASK_DEBUG=1
 
 def read():
-	keywords = open('keywords.txt', 'r') .read()
+	keywords = open('/var/www/stocknesscrawler/keywords.txt', 'r') .read()
 	return keywords
 
 @app.route('/',methods=["GET", "POST"])
@@ -16,8 +18,11 @@ def index():
 		return render_template('index.html', success=False, keywords=keywords)
 	else:
 		data = request.form['keywords']
-		with open('keywords.txt',"wb") as fo:
+		with open('/var/www/stocknesscrawler/keywords.txt',"wb") as fo:
 			fo.write(data)
 			success = True
 		keywords = read()
 		return render_template('index.html', success=True, keywords=keywords)
+
+if __name__ == "__main__":
+	app.run()
--- scrape.py
+++ scrape.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup,SoupStrainer
 import requests,csv,smtplib,datetime
 from email.mime.text import MIMEText
 from os.path import basename
@@ -10,8 +10,9 @@ def send_mail():
 
 	msg = MIMEMultipart()
 	msg['Subject'] = 'Stocknessmonster scrape of %s' % today 
-	msg['From'] = 'derk@uber.com'
-	msg['To'] = 'derkzomer@gmail.com'
+	msg['From'] = 'derkzomer@gmail.com'
+	msg['To'] = 'njwhite@deloitte.com.au'
+	#msg['To'] = 'derkzomer@gmail.com'
 
 	part = MIMEBase('application', "octet-stream")
 	part.set_payload(open(file, "rb").read())
@@ -19,14 +20,14 @@ def send_mail():
 
 	part.add_header('Content-Disposition', 'attachment; filename="'+file+'"')
 
-	text = 'Thanks for the wine.'
+	text = 'Thanks for the wine. You can change keywords at http://stocknesscrawler.derkzomer.com.au/. You get emailed the results every morning at 9:30am. CYC'
 
 	msg.attach(MIMEText(text))
 	msg.attach(part)
 
 	smtp = smtplib.SMTP('smtp.gmail.com',587)
 	smtp.starttls()
-	smtp.login('derkzomer@gmail.com','0vtr1EaujWN1')
+	smtp.login('derkzomer@gmail.com','qm6WOyj4i*53l0')
 	smtp.sendmail(msg['From'], msg['To'], msg.as_string())
 	smtp.close()
 
@@ -37,34 +38,33 @@ soup = BeautifulSoup(request_object.content,"html.parser")
 
 today = datetime.date.today()
 
-file = 'scrape-' + str(today) + '.csv'
+file = '/var/www/stocknesscrawler/scrape-' + str(today) + '.csv'
 
-keywords_file = open('keywords.txt', 'r') .read()
+keywords_file = open('/var/www/stocknesscrawler/keywords.txt', 'r') .read()
 keywords = keywords_file.split(",")
 
 rows = []
 
-for tr in soup.find_all('tr'):
-	data = []
-	for time in tr.find_all('td')[0]:
-		data.append(time)
-	for a in tr.find_all('a',href=True):
-		data.append('http://stocknessmonster.com' + a['href'])
-		data.append(a.text)
+data_table = soup.find_all('table')
 
+
+for tr in data_table[4].find_all('tr'):
+	data = []
+	data.append(tr.find_all('td')[0].text)
+	data.append(tr.find_all('td')[1].text)
+	data.append(tr.find_all('td')[2].text)
+	try:
+		data.append('http://stocknessmonster.com' + tr.find_all('td')[2].find('a')['href'])
+	except:
+		pass
 	rows.append(data)
 
-del rows[:6]
 results = []
 
-try:
-	for row in rows:
-		# print row[4]
-		for keyword in keywords:
-				if keyword in row[4].lower():
-					results.append(row)
-except IndexError:
-	pass
+for row in rows:
+	for keyword in keywords:
+		if keyword.lower() in row[2].lower():
+			results.append(row)
 
 with open(file, "wb") as csv_file:
 	writer = csv.writer(csv_file, delimiter=',')
